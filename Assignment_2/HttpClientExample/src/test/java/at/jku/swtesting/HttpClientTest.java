d5eae4d39a70b386be8ebb503e06e8e24b9390c5
Andrei Blokhin <andrei_blokhin@outlook.com>
2023-03-30 12:29:39 +0200

    Added: Comment on the test we were given with the class A simple mock test that tests the same functionality

--- Assignment_2/HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
+++ Assignment_2/HttpClientExample/src/test/java/at/jku/swtesting/HttpClientTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -13,6 +14,7 @@ import java.net.URL;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +23,15 @@ public class HttpClientTest {
 	@Mock
 	private URL mockURL;
 
+	/**
+	 * Disadvantages of this test:
+	 * 1. The test tests not a single method but both HttpClient and a method of HttpClient class
+	 * 2. Content that we check on the website(title) might change therefore the test might fail even if the site is online, it will fail if the site is down as well
+	 * 3. The test depends on the network connection, if the connection is not available the test will fail as well as it may take longer time to run the
+	 * test if there are some network issues or the connection is slow
+	 * 4. The test is not repeatable, if the website is changed the test will fail even if the code is correct
+	 * 5. The test contains only one check of the title and no other checks(i.e no exceptions from HttpClient could be caught, no coverage if the page is unavailable
+	 */
 	@Test
 	public void testGetContentOnline() throws MalformedURLException {
 		HttpClient myClient = new HttpClient();
@@ -30,7 +41,24 @@ public class HttpClientTest {
 
 	@Test
 	public void testGetContentMocked() throws IOException {
-		// TODO: Implement test using mocks
+
+		//arrange data and mocks
+		String expectedContent = "Hello Mock World!";
+		mockURL = Mockito.mock(URL.class);
+		HttpClient httpClient = new HttpClient();
+
+		//mocks
+		HttpURLConnection mockedHttpUrlConnection = Mockito.mock(HttpURLConnection.class);
+		when(mockURL.openConnection()).thenReturn(mockedHttpUrlConnection);
+
+		InputStream mockedInputStream = new ByteArrayInputStream(expectedContent.getBytes());
+		when(mockedHttpUrlConnection.getInputStream()).thenReturn(mockedInputStream);
+
+		//act
+		String content = httpClient.getContent(mockURL);
+
+		//assert
+		assertEquals(expectedContent, content);
 	}
 
 }
